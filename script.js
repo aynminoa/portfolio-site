e5c8a4c19cb661b9c3efa13b72d5d177659de58f
aynminoa <75557400+aynminoa@users.noreply.github.com>
2021-03-30 14:58:39 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+$('.hello').fadeIn(2000);
+$('.name').fadeIn(2000);
+
+$(function(){
+    $(window).scroll(function (){
+        $('.about-inner, .skills-inner, .works-inner').each(function(){
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 2800){
+                $(this).addClass('scrollin');
+            }
+        });
+    });
+});
+
+$(function(){
+    $('a[href^=#]').click(function(){
+      var adjust = 0;
+      var speed = 600;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top + adjust;
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+      return false;
+    });
+  });
+
+  jQuery(function() {
+    var pagetop = $('#page_top');   
+    pagetop.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {  //100pxスクロールしたら表示
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    });
+    pagetop.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500); //0.5秒かけてトップへ移動
+        return false;
+    });
+});
+
